b7a26a20477e54f215046a34e60bcfd53dd13451
Turbinist <srudyjj@gmail.com>
2019-01-26 18:33:00 +0200

    Add mongo

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodekb', { useNewUrlParser: true });
+let db = mongoose.connection;
+
+db.once('open', () => {
+    console.log("Connected to MongoDB")
+})
+
+db.on('error', (err) => {
+    console.log(err);
+})
 
 // init App
 const app = express();
+
+let Article = require('./models/article');
 
 
 // Load View Engine
@@ -12,9 +26,17 @@ app.set('view engine', 'pug');
 
 // Home route
 app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Articles'
-    });
+    
+    Article.find({}, (err, articles) => { 
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('index', {
+                title: 'Articles',
+                articles: articles
+            });
+        }
+    })
 });
 
 // Add route
