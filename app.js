4d5b4cc27643c603550439d7e8724d557eb74b32
Turbinist <srudyjj@gmail.com>
2019-01-30 17:25:36 +0200

    7 part

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/nodekb', { useNewUrlParser: true });
 let db = mongoose.connection;
@@ -24,6 +25,16 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Body parser middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 // Home route
 app.get('/', (req, res) => {
     
@@ -45,6 +56,72 @@ app.get('/articles/add', (req, res) => {
         title: 'Add Article'
     });
 });
+
+app.post('/articles/add', (req, res) => {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+ 
+    article.save( err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/')
+        }
+    })
+});
+
+app.get('/article/:id', (req, res) => {
+    Article.findById(req.params.id, (err, article) => {
+        res.render('article', {
+            article
+        });
+        
+        return;
+    })    
+})
+
+app.get('/article/edit/:id', (req, res) => {
+    Article.findById(req.params.id, (err, article) => {
+        res.render('edit_article', {
+            title: 'Edit Article',
+            article
+        });
+        
+        return;
+    })    
+})
+
+app.post('/articles/edit/:id', (req, res) => {
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    let query = {_id:req.params.id} 
+ 
+    Article.update(query, article, err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/')
+        }
+    })
+});
+
+app.delete('/article/:id', (req, res) => {
+    let query = {_id:req.params.id};
+    Article.remove(query, (err)=>{
+        if (err) {
+            console.log(err);
+        } 
+        res.send('Success')
+    })
+
+})
 
 // Start server
 app.listen(3000, () => {
---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+    $('.delete-article').on('click', (e) => {
+        $target = $(e.target);
+        const id = $target.attr('data-id');
+        $.ajax({
+            type: 'DELETE',
+            url: '/article/'+id,
+            success: (res) => {
+                alert('Deleting Article');
+                window.location.href = '/';
+            },
+            error: (err) => {
+                console.log(id, err.statusText);
+            }
+        })
+        
+    })
+})
